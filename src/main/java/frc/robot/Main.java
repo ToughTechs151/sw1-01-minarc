3efd242df3f296c13f4639a99bb46ccb16f2d128
Brian Utterback <brian@utterback.org>
2020-12-09 14:43:28 -0500

    Add comments.

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -14,8 +14,16 @@ public final class Main {
   /**
    * Main initialization function. Do not perform any initialization here.
    *
-   * <p>If you change your main robot class, change the parameter type.
+   * If you change your main robot class, change the parameter type.
    */
+  
+  /**
+   * This is the standard main function as required by Java. The JVM looks for
+   * this method as the starting point for execution.
+   * We instanstiate a new robot from our Robot class in the Robot.java file and
+   * pass it into the startRobot method from WPIlib. WPILib will do all the
+   * required initialization of the Roborio or simulator, depending on the 
+   * environment we are running on.
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
